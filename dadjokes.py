b46d3bd83eea87e88e5404a85f4ab19c8472ee03
KevinH <khui98@gmail.com>
2024-08-01 12:58:37 -0400

    Updated python and html
    Updated python and html and added requirements.txt

--- dadjokes.py
+++ dadjokes.py
@@ -1,17 +1,20 @@
 import requests
 import os.path
 
+
 def joke():
     """Parses API from icanhazdadjoke.com and returns a random joke"""
+    # Gets joke from website API
     r = requests.get('https://icanhazdadjoke.com', headers={"Accept":"application/json"})
     raw_joke = r.json()
     joke = raw_joke['joke']
 
+    # Creates path relative to workspace
     path = os.path.abspath(os.path.dirname(__file__))
     filepath = os.path.join(path, "./templates/joke.txt")
     
-    # Writes joke for joke.txt in plain text
     with open(filepath, 'w') as f:
-        f.write("Joke: "+ str(joke))
+        f.write("Joke: "+ str(joke) + "\n")
+        f.write("From https://icanhazdadjoke.com/")
 
     return joke
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request
 from flask_mail import Mail, Message
-import os
-import dadjokes
+import os, dadjokes, random
 
 app = Flask(__name__)
 
-# Sets configuration for app
 app.config.update(dict(
     SECRET_KEY = os.urandom(12),
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{app.root_path}/instance/email.db",
@@ -19,32 +17,55 @@ app.config.update(dict(
 
 mail = Mail(app)
 
+
+def rand_images():
+    """returns a random absolute URL of an image"""
+    image_list = ["https://images.pexels.com/photos/2337789/pexels-photo-2337789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/1115680/pexels-photo-1115680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/1183434/pexels-photo-1183434.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://i.kym-cdn.com/entries/icons/original/000/014/959/Screenshot_116.png",
+            "https://cdn.pixabay.com/photo/2014/03/24/13/43/cat-294120_960_720.png"
+            ]
+    
+    image = random.choice(image_list)
+
+    return image
+
+
 def send_email(rec_email):
     """Sends email as txt and html files"""
     joke = dadjokes.joke()                                  # Gets a joke from dadjokes.py
+    img_tag = rand_images()                                 # Gets a random image from rand_images()
 
     msg = Message("Here Have A Joke!",
                     sender=app.config["MAIL_USERNAME"], 
-                    recipients=[rec_email])                 # Make sure to send recipients as a list
+                    recipients=[rec_email])                 # Make sure to have recipients in a list
 
 
-    msg.body = render_template("joke.txt")                  # text file to contain the body
-    msg.html = render_template("joke.html", joke=joke)      # html file to contain the elements, html has priority over body
+    # text file to contain the body, 
+    # will display if html file is unable to load
+    msg.body = render_template("joke.txt")                                      
+    
+    # html file to contain the elements and variables
+    msg.html = render_template("joke.html", joke=joke, img_tag=img_tag)         
 
-    mail.send(msg)                                          # Sends msg to recipient email
+    mail.send(msg)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
+
 @app.route('/success', methods=["POST"])  
 def verify():  
+    """Sends email with pun and image"""
     email = request.form["email"]
     send_email(email)
 
     return render_template('success.html')
  
+
 if __name__ == "__main__":
     with app.app_context():
         app.run(debug=True, port=5001)        # Only runs on local 127.0.0.1 network
